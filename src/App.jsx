f82b58ae975a92f8735fb5fb1d3912967a756981
Tas <164704954+Tas-r@users.noreply.github.com>
2025-04-23 01:51:43 -0400

    index

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import OurPractice from "./pages/index/OurPractice";
 import CommunityOutreach from "./pages/index/CommunityOutreach";
-import Services from "./pages/index/services";
+import Services from "./pages/index/Services";
 import Technology from "./pages/index/Technology";
 import AdditionalInfo from "./pages/index/AdditionalInfo";
 import Login from "./pages/Login";
